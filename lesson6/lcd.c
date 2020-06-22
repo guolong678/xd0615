8429549d9adedf7e32ca03d020da6faee93d3ca9
guolong678 <2073718673@qq.com>
2020-06-22 17:33:50 +0800

    这是我的第六次作业 by郭龙 19050100114

---
+++ lesson6/lcd.c
@@ -0,0 +1,11 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+void setup() 
+  lcd.begin(16, 2);
+  lcd.print("happy,my ");
+}
+void loop() {
+  lcd.setCursor(1, 1);
+  lcd.print(millis() / 1000);
+}
+ 
